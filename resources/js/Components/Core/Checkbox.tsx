68120bb9e489e1279fbdbefcc5c46216a0aa8d65
Rahul Rajput <rahul242rajput@gmail.com>
2025-01-09 18:09:57 -0500

    first commit

---
+++ resources/js/Components/Core/Checkbox.tsx
@@ -0,0 +1,18 @@
+import { InputHTMLAttributes } from 'react';
+
+export default function Checkbox(
+  {
+    className = '',
+    ...props
+}: InputHTMLAttributes<HTMLInputElement>) {
+    return (
+        <input
+            {...props}
+            type="checkbox"
+            className={
+                'checkbox checkbox-primary' +
+                className
+            }
+        />
+    );
+}
