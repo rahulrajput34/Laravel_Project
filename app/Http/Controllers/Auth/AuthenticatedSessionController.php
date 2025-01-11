afb9d72c45004f733428c80c6c973f0e67b7185c
Rahul Rajput <rahul242rajput@gmail.com>
2025-01-10 20:21:37 -0500

    add Department model, migration, and Filament admin panel provider; implement role-based access control middleware

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\RolesEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class AuthenticatedSessionController extends Controller
 {
+
     /**
      * Display the login view.
      */
@@ -24,16 +27,32 @@ class AuthenticatedSessionController extends Controller
         ]);
     }
 
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function store(LoginRequest $request): RedirectResponse
+  /**
+   * Handle an incoming authentication request.
+   * @throws ValidationException
+   */
+    public function store(LoginRequest $request): \Symfony\Component\HttpFoundation\Response
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        //TODO: This is how we can login of the different access for different people
+        $user = Auth::user();
+        $route = "/";
+
+        if($user->hasAnyRole([RolesEnum::Admin, RolesEnum::Vendor])){
+            // For the above action Inertia will redirect the user to the admin path
+            // Here first we passed the path /admin but do not want to pass direct the path
+            // for that get the list or routes and find the admin path by ctrl+f and pass /admin and pass the path below
+            // Passing difference ==> ('/admin') , route('filament.admin.pages.dashboard')
+            return Inertia::location(route('filament.admin.pages.dashboard'));
+        } else {
+            // For the above action Inertia will redirect the user to the customer path
+            $route = route('dashboard', absolute: false);
+        }
+
+        return redirect()->intended($route);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// Here we passed name('dashboard') because
+// Here the dashboard is the home page that is why I passed it to home route
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -12,11 +14,7 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -24,4 +22,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
